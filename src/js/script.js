f3550f9a21dd48e2da5dab6bb3d831d0ff832cdd
Artem <sven-rus@mail.ru>
2023-01-14 17:37:11 +0300

    add modal

--- src/js/script.js
+++ src/js/script.js
@@ -45,4 +45,17 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-  });
+       //modal
+    $('[data-modal="consultation"]').on('click', function(){
+      $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+    $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn();
+      })
+    });
+});
